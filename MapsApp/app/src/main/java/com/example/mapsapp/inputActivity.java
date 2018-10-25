e6b000e51c8c1c0727daf154c7f6cee11ef5581b
Sopheara <sduong1125@gmail.com>
2018-10-24 21:56:01 -0500

    added MapsApp
    added Homework 4 Android App

---
+++ MapsApp/app/src/main/java/com/example/mapsapp/inputActivity.java
@@ -0,0 +1,29 @@
+package com.example.mapsapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class inputActivity extends AppCompatActivity {
+
+    public final static String EXTRA_Message = "com.example.inputActivity.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+    }
+
+    public void sendInput(View view){
+        Intent intent = new Intent(this, MapsActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
